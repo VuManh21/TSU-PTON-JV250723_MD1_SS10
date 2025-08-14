f3b9456c835687ef177caf179ed4e1c0ffe6b6ff
VuManh21 <87159958+VuManh21@users.noreply.github.com>
2025-08-14 23:54:45 +0900

    TSU-PTON-JV250723_MD1_SS10_Bai04

---
+++ MD1_SS10_bai4/main.js
@@ -0,0 +1,6 @@
+let firstNumber = Number(prompt( "nhap so  "));
+let secondNumber = Number(prompt( "nhap so  "));
+ 
+let tongHaiSo = firstNumber + secondNumber;
+
+alert("tong 2 so la " + tongHaiSo);
