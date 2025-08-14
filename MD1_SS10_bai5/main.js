daad63bfaaec7f50837198ae8bc4ee613ef66f64
VuManh21 <87159958+VuManh21@users.noreply.github.com>
2025-08-14 23:54:59 +0900

    TSU-PTON-JV250723_MD1_SS10_Bai05

---
+++ MD1_SS10_bai5/main.js
@@ -0,0 +1,4 @@
+let tienDoLaMy = Number(prompt("nhap so tien $ can doi"));
+let tigiaTienViet = 25000;
+  let soTienVietDuocDoi = tienDoLaMy * tigiaTienViet;
+alert(`so tien doi duoc: ${soTienVietDuocDoi}`);
